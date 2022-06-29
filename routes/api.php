9e55d2b0ad68423b6efdb892fedc4c4441618e68
Ed Tervit <edtervit@hotmail.co.uk>
2022-06-29 20:25:22 +0100

    basic test message route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MessagesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,6 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('test', function () {
-    return 'hello 1';
-});
+Route::get('messages/photography', [MessagesController::class, 'getPhotographyMessages']);
